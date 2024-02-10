6ba25b2b4dfb96a5cde440adebd9e8780acae4c3
Mekintosz <134439256+Mekintosz@users.noreply.github.com>
2024-02-10 03:07:08 +0100

    fetch weatherApi, format weather data

--- script.js
+++ script.js
@@ -1,9 +1,46 @@
-async function provideWeatherData() {
-  
-    const response = await fetch('http://api.weatherapi.com/v1/current.json?key=ad519565f97d430fb67163741240602&q=wrolaw&aqi=no')
-    const weatherData = await response.json()
-    return weatherData
-
+async function provideWeatherData(location = 'Wroclaw') {
+  const apiCall = `http://api.weatherapi.com/v1/current.json?key=ad519565f97d430fb67163741240602&q=${location}&aqi=yes`;
+  try {
+    const response = await fetch(apiCall, { mode: "cors" });
+    if (!response.ok) throw new Error(`Location ${location} not found`);
+    const weatherData = convertData(await response.json());
+    return weatherData;
+  } catch (err) {
+    alert(err);
+    return null;
+  }
 }
 
-console.log(provideWeatherData())
+function convertData(data) {
+  const {
+    location: { name, country, lat, lon },
+    current: {
+      temp_c,
+      feelslike_c,
+      temp_f,
+      feelslike_f,
+      wind_mph,
+      wind_kph,
+      humidity,
+      condition: { text, code },
+      air_quality: { co, no2, o3, so2, pm2_5, pm10 }
+    }
+  } = data;
+  return {
+    location: { name, country, lat, lon },
+    current: {
+      temp_c,
+      feelslike_c,
+      temp_f,
+      feelslike_f,
+      wind_mph,
+      wind_kph,
+      humidity,
+      condition: { text, code },
+      air_quality: { co, no2, o3, so2, pm2_5, pm10 }
+    }
+  }
+}
+
+const data = provideWeatherData('klodzko');
+console.log(data);
