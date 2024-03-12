27383eca745880feaa4399696e8ac0171329b95e
Mekintosz <134439256+Mekintosz@users.noreply.github.com>
2024-03-12 18:19:11 +0100

    ready for deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,4 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 
 module.exports ={
   entry: './src/PogodaApp.js',
@@ -9,34 +7,12 @@ module.exports ={
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  // devtool: 'inline-source-map', //good for development not for deployment
   module: {
     rules: [
-        {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-        },
-        {
-            test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            type: 'asset/resource',
-        },
-        {
-          test: /\.(woff|woff2|eot|ttf|oft)$/i,
-          type: 'asset/resource',
-        },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: 'index.html',
-      inject: "head",
-      scriptloading: 'defer',
-    }),
-    plugins: [
-      new FaviconsWebpackPlugin({
-        logo: './src/assets/favicon.svg',
-      }) // svg works too!
+      {
+        test: /\.(png|svg|jpg)$/i,
+        type: 'asset/resource',
+      },
     ]
-  ]
-};
+  }
+}
